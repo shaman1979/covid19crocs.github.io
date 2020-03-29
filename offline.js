eab887c6d181f927c2180b10d87ce723bb179237
shaman1979 <31655538+shaman1979@users.noreply.github.com>
2020-03-29 14:09:17 +0300

    Добавил файлы сайта

---
+++ offline.js
@@ -0,0 +1,23 @@
+﻿{
+	"version": 1585478807,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/enemy-sheet0.png",
+		"images/enemy2-sheet0.png",
+		"images/enemy2-sheet1.png",
+		"images/enemy2-sheet2.png",
+		"images/startgamebutton-sheet0.png",
+		"images/informationbar-sheet0.png",
+		"images/border-sheet0.png",
+		"images/tapka-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
